7c5e88398574d631ff285c0c9e121e39e24ddb04
tmampa <tmampa94@gmail.com>
2023-11-22 15:19:32 +0200

    Works well

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -13,6 +13,6 @@ export class ApiService {
     return this.http.get(this.rootURL + '/users');
   }
   addUser(user: any) {
-    return this.http.post(this.rootURL + '/user', {user});
+    return this.http.post(this.rootURL + '/users', {user});
   }
 }
